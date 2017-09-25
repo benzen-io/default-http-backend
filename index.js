8ffe12f6889e6fd606f22a07bc26b685171d6323
dehypnosis <dehypnosis@gmail.com>
2017-09-25 12:16:01 +0900

    build cache enabled, last check not overlayed

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get('/healthz', (req,res) => {
-	res.status(200).end();
+	res.status(200).end()
 });
 
 app.get('/', (req,res) => {
-	res.send('Hello World!')
+	res.send('Hello World 2')
 });
 
 app.listen(8080, () => {
-	console.log('Run on 8080!');
+	console.log('Run on 8080!')
 });
